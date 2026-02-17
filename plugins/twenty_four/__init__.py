0df57db3c8ae1115b3155db36490a94f2cfaf994
hyouryo <3433609429@qq.com>
2026-02-17 12:19:01 +0800

    24点群聊版，没有适配私聊

--- plugins/twenty_four/__init__.py
+++ plugins/twenty_four/__init__.py
@@ -7,9 +7,10 @@ from time import time
 
 from aiofiles import open  # noqa: A004
 from mathparse import mathparse
-from nonebot import on_command, on_message
+from nonebot import on_message, on_command
 from nonebot.adapters import Event
 from nonebot.plugin import PluginMetadata
+from nonebot.matcher import Matcher
 
 from ...tools import get_group_id, get_sender_nickname
 from .utils import format_expression
@@ -40,7 +41,7 @@ class TwentyFourGame:
         return match == self.numbers
 
 
-twenty_four_start = on_command("24点")
+twenty_four_start = on_command("24点",priority=10)
 twenty_four_games: dict[str, TwentyFourGame] = {}
 
 after_twenty_four_time = 0
@@ -52,7 +53,10 @@ answer_path = Path(__file__).parent / "answer.json"
 @twenty_four_start.handle()
 async def twenty_four_handle(event: Event) -> None:
     # 获取群号
-    group_id = get_group_id(event)
+    try:
+        group_id = get_group_id(event)
+    except ValueError:
+        await twenty_four_start.finish("本功能仅支持群组使用")
     # 检查群是否已存在游戏
     if group_id in twenty_four_games:
         game = twenty_four_games[group_id]
@@ -65,12 +69,12 @@ async def twenty_four_handle(event: Event) -> None:
     _dict: dict = json.loads(file)  # 类似 {"2 6 7 8":"(2+7-6)x8","2 6 7 8":"(2+7-6)x8"}
     _list: list = list(_dict.keys())  # 类似 ["2 6 7 8","2 6 7 8"]
     # 随机选择一个可用的24点数字
-    que_str: str = random.choice(_list) # 类似 "2 6 7 8"
+    que_str: str = random.choice(_list)  # 类似 "2 6 7 8"
     # 将数字切片并排序
-    nums = que_str.split() # 类似 [2, 6, 7, 8]
+    nums = que_str.split()  # 类似 [2, 6, 7, 8]
     nums.sort()
     # 获取答案
-    answer = _dict[que_str] # 类似 "(2+7-6)x8"
+    answer = _dict[que_str]  # 类似 "(2+7-6)x8"
 
     # 创建游戏
     game = TwentyFourGame(group_id, nums, answer)
@@ -82,13 +86,16 @@ async def twenty_four_handle(event: Event) -> None:
     )
 
 
-get_twenty_four_answer = on_message(priority=10, block=False)
+get_twenty_four_answer = on_message(priority=1, block=False)
 
 
 @get_twenty_four_answer.handle()
-async def _(event: Event) -> None:
+async def _(event: Event, matcher:Matcher) -> None:
     # 获取群号
-    group_id = get_group_id(event)
+    try:
+        group_id = get_group_id(event)
+    except ValueError:
+        return
     # 检查游戏列表中是否有该群的游戏
     if group_id not in twenty_four_games:
         return
@@ -97,6 +104,7 @@ async def _(event: Event) -> None:
         await get_twenty_four_answer.finish("24点游戏时间到~")
     submit = event.get_message().extract_plain_text().strip()
     if submit == "24点提示":
+        matcher.stop_propagation()
         await get_twenty_four_answer.finish(f"可以得到24点的式子：{game.answer}")
     format_ = format_expression(submit)
     try:
