5b388627cb34e6d2e80a6c6abbf7a0e9063f5537
梦帘 <180162404@qq.com>
2026-02-17 20:24:17 +0800

    update

--- plugins/twenty_four/__init__.py
+++ plugins/twenty_four/__init__.py
@@ -7,10 +7,9 @@ from time import time
 
 from aiofiles import open  # noqa: A004
 from mathparse import mathparse
-from nonebot import on_message, on_command
+from nonebot import on_command
 from nonebot.adapters import Event
 from nonebot.plugin import PluginMetadata
-from nonebot.matcher import Matcher
 
 from ...tools import get_group_id, get_sender_nickname
 from .utils import format_expression
@@ -82,35 +81,53 @@ async def twenty_four_handle(event: Event) -> None:
     twenty_four_games[group_id] = game
 
     await twenty_four_start.finish(
-        f"当前题目：{que_str}\n使用 算式 来回答结果\n可以使用+-*/和()\n例：(1+2)*3/4 即可回答"  # noqa: E501
+        f"当前题目：{que_str}\n使用 24答 算式 来回答结果\n可以使用+-*/和()\n例：24答 (1+2)*3/4\n发送 24点提示 可查看答案"  # noqa: E501
     )
 
 
-get_twenty_four_answer = on_message(priority=1, block=False)
+twenty_four_hint = on_command("24点提示", priority=10)
+
+
+@twenty_four_hint.handle()
+async def twenty_four_hint_handle(event: Event) -> None:
+    try:
+        group_id = get_group_id(event)
+    except ValueError:
+        await twenty_four_hint.finish("本功能仅支持群组使用")
+    if group_id not in twenty_four_games:
+        await twenty_four_hint.finish("当前没有进行中的24点游戏哦")
+    game = twenty_four_games[group_id]
+    if game.is_timeout():
+        twenty_four_games.pop(group_id)
+        await twenty_four_hint.finish("24点游戏已超时~")
+    await twenty_four_hint.finish(f"可以得到24点的式子：{game.answer}")
+
+
+get_twenty_four_answer = on_command("24答", aliases={"24点答题"}, priority=10)
 
 
 @get_twenty_four_answer.handle()
-async def _(event: Event, matcher:Matcher) -> None:
+async def twenty_four_answer_handle(event: Event) -> None:
     # 获取群号
     try:
         group_id = get_group_id(event)
     except ValueError:
-        return
+        await get_twenty_four_answer.finish("本功能仅支持群组使用")
     # 检查游戏列表中是否有该群的游戏
     if group_id not in twenty_four_games:
-        return
+        await get_twenty_four_answer.finish("当前没有进行中的24点游戏哦")
     game = twenty_four_games[group_id]
     if game.is_timeout():
+        twenty_four_games.pop(group_id)
         await get_twenty_four_answer.finish("24点游戏时间到~")
     submit = event.get_message().extract_plain_text().strip()
-    if submit == "24点提示":
-        matcher.stop_propagation()
-        await get_twenty_four_answer.finish(f"可以得到24点的式子：{game.answer}")
+    if not submit:
+        await get_twenty_four_answer.finish("请在命令后输入算式，例：24答 (1+2)*3/4")
     format_ = format_expression(submit)
     try:
         answer_ = mathparse.parse(format_)  # 数
     except Exception:  # noqa: BLE001
-        return
+        await get_twenty_four_answer.finish("算式格式不正确，请检查后重试")
     if type(answer_) is float | Decimal:
         answer_ = round(answer_, 2)
     match = re.findall(r"(\d+)", submit)
