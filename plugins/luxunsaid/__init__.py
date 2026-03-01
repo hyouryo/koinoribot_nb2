692a641832a251c88f02522b6e06daa946953945
hyouryo <3433609429@qq.com>
2026-03-01 20:51:27 +0800

    鲁迅说过修复图片路径问题

--- plugins/luxunsaid/__init__.py
+++ plugins/luxunsaid/__init__.py
@@ -39,7 +39,7 @@ async def luxunsaid_handle(event: Event, msg: Message = CommandArg()) -> None:
     for biaodian in [",", "，", ":", "："]:
         if message.startswith(biaodian):
             message.strip(biaodian)
-    image_file = Path(__file__).parent / "luxunsaid.png"
+    image_file = Path(__file__).parent / "luxun.png"
     bg = BuildImage(0, 0, font_size=37, background=image_file, font="STKAITI.TTF")
     say = ""
     if len(message) > 40:  # noqa: PLR2004
