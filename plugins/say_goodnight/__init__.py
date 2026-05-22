64811379ee718efcc4a22acdf743986595dcc382
梦帘 <180162404@qq.com>
2026-05-23 06:30:27 +0800

    fix 晚安

--- plugins/say_goodnight/__init__.py
+++ plugins/say_goodnight/__init__.py
@@ -1,6 +1,8 @@
 from nonebot import on_command
 from nonebot.params import Depends
 from nonebot.plugin import PluginMetadata
+from nonebot.adapters.onebot.v11 import Bot, Event
+import time
 
 from ...money import UserWallet, wallet_manager
 
@@ -10,14 +12,29 @@ __plugin_meta__ = PluginMetadata(
     usage="说晚安"
 )
 
-
 say_goodnight = on_command("晚安")
+
+cooldown = {}
 
 
 @say_goodnight.handle()
 async def say_goodnight_handle(
+    bot: Bot,
+    event: Event,
     user_wallet: UserWallet = Depends(wallet_manager),
 ) -> None:
-    if user_wallet.gold >= 50:  # noqa: PLR2004
+    user_id = event.get_user_id()
+    
+    if user_id == str(bot.self_id):
+        return
+    
+    now = time.time()
+    if user_id in cooldown and now - cooldown[user_id] < 10:
+        return
+    cooldown[user_id] = now
+    
+    if user_wallet.gold >= 50:
         user_wallet.gold -= 50
         await say_goodnight.finish("晚安喵")
+    else:
+        return
